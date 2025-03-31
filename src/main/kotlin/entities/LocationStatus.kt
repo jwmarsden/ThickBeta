b2d2609454028bba34d0213eec18224446094e2c
J.W.Marsden <j.w.marsden@gmail.com>
2025-03-31 22:02:10 +1030

    Refactor, Add Hibernate

--- src/main/kotlin/entities/LocationStatus.kt
+++ src/main/kotlin/entities/LocationStatus.kt
@@ -1,15 +1,27 @@
 package com.ventia.entities
 
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "LOCATION_STATUS")
 data class LocationStatus(
 
+    @Id
+    @Column(nullable = false)
     val id: Int,
 
+    @Column(nullable = false)
     var systemStatus: String,
 
+    @Column(nullable = false)
     var status: String,
 
-    var description: String,
+    val description: String,
 
+    @Column(nullable = false)
     val active: Boolean,
 
 )
